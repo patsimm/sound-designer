2cc205e74f20c85ad5a47cc13e6bdec4cfbf2f21
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2024-11-11 17:39:27 +0100

    change indicator color 🩷

--- src/Indicator.tsx
+++ src/Indicator.tsx
@@ -5,11 +5,11 @@ import { useAppStore } from "./App.store.ts";
 export function Indicator() {
   const pos = useAppStore((state) => state.indicatorPos);
   return (
-    <div
-      className={classNames("time-indicator__container")}
-      style={{ transform: `translateX(${pos}px)` }}
-    >
-      <div className={classNames("time-indicator__indicator")}></div>
+    <div className={classNames("time-indicator__container")}>
+      <div
+        className={classNames("time-indicator__indicator")}
+        style={{ transform: `translateX(${pos}px)` }}
+      ></div>
     </div>
   );
 }
